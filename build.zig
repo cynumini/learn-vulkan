32998625193da52221936a0bf9bc356ad3bb9223
cynumini <cynumini@proton.me>
2026-07-06 17:11:44 +0300

    restart from "Development environment"

--- build.zig
+++ build.zig
@@ -3,29 +3,48 @@ const std = @import("std");
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
+
+    const translate = b.addTranslateC(.{
+        .root_source_file = b.path("src/c.h"),
+        .target = target,
+        .optimize = optimize,
+    });
+
     const exe = b.addExecutable(.{
         .name = "learn_vulkan",
         .root_module = b.createModule(.{
             .root_source_file = b.path("src/main.zig"),
-            .link_libc = true,
             .target = target,
             .optimize = optimize,
+            .link_libcpp = true,
+            .imports = &.{
+                .{ .name = "c", .module = translate.addModule("c") },
+            },
         }),
     });
-    exe.linkSystemLibrary("glfw");
-    exe.linkSystemLibrary("vulkan");
+
+    exe.root_module.addCSourceFile(.{ .file = b.path("src/glm.cpp") });
+
+    exe.root_module.linkSystemLibrary("glfw3", .{});
+    exe.root_module.linkSystemLibrary("vulkan", .{});
+
     b.installArtifact(exe);
+
     const run_step = b.step("run", "Run the app");
+
     const run_cmd = b.addRunArtifact(exe);
     run_step.dependOn(&run_cmd.step);
+
     run_cmd.step.dependOn(b.getInstallStep());
-    if (b.args) |args| {
-        run_cmd.addArgs(args);
-    }
+
+    run_cmd.addPassthruArgs();
+
     const exe_tests = b.addTest(.{
         .root_module = exe.root_module,
     });
+
     const run_exe_tests = b.addRunArtifact(exe_tests);
+
     const test_step = b.step("test", "Run tests");
     test_step.dependOn(&run_exe_tests.step);
 }
--- src/main.zig
+++ src/main.zig
@@ -1,15 +1,28 @@
 const std = @import("std");
+const c = @import("c");
 
-const HelloTriangleApplication = @import("hello_triangle_application.zig");
-const math = @import("math.zig");
-const vk = @import("vulkan.zig");
+pub extern fn lv_glm_test() void;
 
-pub fn main() !void {
-    var da: std.heap.DebugAllocator(.{}) = .init;
-    defer _ = da.deinit();
-    const allocator = da.allocator();
+pub fn main(init: std.process.Init) !void {
+    _ = init;
 
-    var app: HelloTriangleApplication = .{.allocator = allocator};
+    _ = c.glfwInit();
+    defer c.glfwTerminate();
 
-    try app.run();
+    c.glfwWindowHint(c.GLFW_CLIENT_API, c.GLFW_NO_API);
+
+    const window = c.glfwCreateWindow(800, 600, "Vulkan window", null, null).?;
+    defer window.glfwDestroyWindow();
+
+    var extensionCount: u32 = 0;
+    _ = c.vkEnumerateInstanceExtensionProperties(null, &extensionCount, null);
+
+    std.debug.print("{} extensions supported\n", .{extensionCount});
+
+    lv_glm_test();
+
+    while(window.glfwWindowShouldClose() == 0) {
+        c.glfwPollEvents();
+    }
+
 }
